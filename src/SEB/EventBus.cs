c7eb7690a9f7b14d20673af7d0a612f76cfc41b8
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-06-02 22:49:05 +0300

    Revert "Add EventListenersCache"
    This reverts commit c9eb65ea170034be079c11bf1f8fbde11b798b1f.

--- src/SEB/EventBus.cs
+++ src/SEB/EventBus.cs
@@ -4,58 +4,39 @@ using System;
 using System.Collections.Generic;
 namespace SEB;
 public class EventBus : IEventBus {
- private readonly Dictionary<Type, EventListenersCache> m_CachedListeners = [];
+ private readonly List<IEventListener> m_Listeners = [];
  public IDisposable Subscribe(IEventListener lisener) {
   this.InternalAdd(lisener);
-  return this.InternalCreateDisposable(lisener);
+  return this.CreateDisposable(lisener);
  }
  public void Unsubscribe(IEventListener lisener) {
   this.InternalRemove(lisener);
  }
  public void Emit(IEvent reason) {
-  if(this.m_CachedListeners.TryGetValue(reason.Type, out var cache)) {
-   cache.Emit(reason);
+  foreach(var listener in this.m_Listeners) {
+   var type = reason.Type;
+   if(type.IsAssignableFrom(listener.EventType)) {
+    listener.Listen(reason);
+   }
   }
  }
  private void InternalAdd(IEventListener listener) {
-  this.InternalGetOrCreateCache(listener.EventType).Cache(listener);
+  if(!this.m_Listeners.Contains(listener)) {
+   this.m_Listeners.Add(listener);
+   this.m_Listeners.Sort();
+  }
  }
  private void InternalRemove(IEventListener listener) {
-  this.InternalGetOrCreateCache(listener.EventType).Clear(listener);
- }
- private EventListenersCache InternalGetOrCreateCache(Type type) {
-  if(this.m_CachedListeners.TryGetValue(type, out var cache)) {
-   return cache;
+  if(this.m_Listeners.Remove(listener)) {
+   this.m_Listeners.Sort();
   }
-  cache = new();
-  this.m_CachedListeners.Add(type, cache);
-  return cache;
  }
- private EventListenerDisposeble InternalCreateDisposable(IEventListener listener) {
+ private EventListenerDisposeble CreateDisposable(IEventListener listener) {
   return new EventListenerDisposeble(this, listener);
  }
  private class EventListenerDisposeble(IEventBus bus, IEventListener listener) : IDisposable {
   public void Dispose() {
    bus.Unsubscribe(listener);
-  }
- }
- private class EventListenersCache : IEventEmitter {
-  private readonly List<IEventListener> m_Cache = [];
-  public void Emit(IEvent reason) {
-   foreach(var listener in this.m_Cache) {
-    listener.Listen(reason);
-   }
-  }
-  internal void Cache(IEventListener listener) {
-   if(!this.m_Cache.Contains(listener)) {
-    this.m_Cache.Add(listener);
-    this.m_Cache.Sort();
-   }
-  }
-  internal void Clear(IEventListener listener) {
-   if(this.m_Cache.Remove(listener)) {
-    this.m_Cache.Sort();
-   }
   }
  }
 }
