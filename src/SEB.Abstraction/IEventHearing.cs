91102cd02277dc040a68e24d9ba3b59002ccceae
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-06-03 01:08:09 +0300

    Add IEventHearing

---
+++ src/SEB.Abstraction/IEventHearing.cs
@@ -0,0 +1,7 @@
+﻿namespace SEB.Abstraction;
+public interface IEventHearing {
+ bool Hear(IEvent reason);
+}
+public interface IEventHearing<TEvent> : IEventHearing where TEvent : IEvent {
+ bool Hear(TEvent reason);
+}
