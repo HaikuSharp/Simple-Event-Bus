96b4db49a2300931a16f54701d300259363b4921
WrongSell <84908244+R2R2HS@users.noreply.github.com>
2025-06-08 15:45:10 +0300

    IEventSource Generic Version

--- src/SEB.Abstraction/IEventSource.cs
+++ src/SEB.Abstraction/IEventSource.cs
@@ -2,3 +2,6 @@
 public interface IEventSource {
  IEvent CreateReason();
 }
+public interface IEventSource<TEvent> : IEventSource where TEvent : IEvent {
+ new TEvent CreateReason();
+}
---
+++ src/SEB/EventSourceBase.cs
@@ -0,0 +1,8 @@
+﻿using SEB.Abstraction;
+namespace SEB;
+public abstract class EventSourceBase<TEvent> : IEventSource<TEvent> where TEvent : IEvent {
+ public abstract TEvent CreateReason();
+ IEvent IEventSource.CreateReason() {
+  return this.CreateReason();
+ }
+}
